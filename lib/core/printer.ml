7256fc700613f40b44a3255cc1ad6a0a15eef1b3
Guillaume Huysmans <ghuysmans99@gmail.com>
2019-09-08 15:16:18 +0200

    ppx_expect

--- lib/core/printer.ml
+++ lib/core/printer.ml
@@ -41,3 +41,64 @@ module Make (I : S) = struct
     to_buffer ?line_prefix buf x;
     Buffer.contents buf
 end
+
+module Tests = struct
+  type t =
+    | Node of string * t * t
+    | Leaf
+
+  module M = struct
+    type nonrec t = t
+
+    let to_string = function
+      | Leaf -> "."
+      | Node (name, _, _) -> name
+
+    let get_children = function
+      | Leaf -> []
+      | Node (_, a, b) -> List.filter ((<>) Leaf) [a; b]
+  end
+
+  module N = Make (M)
+
+  let%expect_test _ =
+    let shared_node =
+      Node (
+        "hello",
+        Node ("world", Leaf, Leaf),
+        Node ("you", Leaf, Leaf)
+      )
+    in
+    let tree =
+      Node (
+        "root",
+        Node (
+          "Mr. Poopypants",
+          Node (
+            "something something",
+            shared_node,
+            Leaf
+          ),
+          Node (
+            "Ms. Poopypants",
+            Leaf,
+            Leaf
+          )
+        ),
+        shared_node
+      )
+    in
+    print_endline @@ N.to_string ~line_prefix:"* " tree;
+    [%expect {|
+  * root
+  * ├── Mr. Poopypants
+  * │   ├── something something
+  * │   │   └── hello
+  * │   │       ├── world
+  * │   │       └── you
+  * │   └── Ms. Poopypants
+  * └── hello
+  *     ├── world
+  *     └── you
+    |}]
+end
